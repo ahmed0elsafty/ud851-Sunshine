fcde1aa0758a8c01a72f84d1104734d7d7f39eb0
El-Safty <afawzy629@gmail.com>
2020-06-02 13:16:35 +0200

    Solve S06.01-Exercise-LaunchSettingsActivity

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -25,6 +25,7 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.TintTypedArray;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -333,24 +334,20 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
-        if (id == R.id.action_refresh) {
-            invalidateData();
-            getSupportLoaderManager().restartLoader(FORECAST_LOADER_ID, null, this);
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_refresh:
+                invalidateData();
+                getSupportLoaderManager().restartLoader(FORECAST_LOADER_ID, null, this);
+                return true;
+            case R.id.action_map:
+                openLocationInMap();
+                return true;
+            case R.id.action_settings:
+                Intent intent =new Intent(this,SettingsActivity.class);
+                startActivity(intent);
+                return true;
         }
-
-        if (id == R.id.action_map) {
-            openLocationInMap();
-            return true;
-        }
-
-        // TODO (1) Add new Activity called SettingsActivity using Android Studio wizard
-        // Do step 2 in SettingsActivity
-        // TODO (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
-
-        // TODO (6) Launch SettingsActivity when the Settings option is clicked
 
         return super.onOptionsItemSelected(item);
     }
---
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+           onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
