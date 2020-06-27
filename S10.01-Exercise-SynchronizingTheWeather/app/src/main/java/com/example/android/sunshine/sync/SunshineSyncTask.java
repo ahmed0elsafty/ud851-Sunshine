873dd59587723d374012cd2f661cf3111030dcea
El-Safty <afawzy629@gmail.com>
2020-06-27 02:37:13 +0200

    Solve S10.01-Exercise-SynchronizingTheWeather

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,36 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.data.WeatherContract.WeatherEntry;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import java.net.URL;
+
+public class SunshineSyncTask {
+    synchronized public static void syncWeather(Context context) {
+
+        URL weatherRequestUrl = NetworkUtils.getUrl(context);
+
+        try {
+            String jsonWeatherResponse = NetworkUtils
+                    .getResponseFromHttpUrl(weatherRequestUrl);
+
+            ContentValues[] simpleJsonWeatherData = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, jsonWeatherResponse);
+
+            if (simpleJsonWeatherData != null && simpleJsonWeatherData.length != 0) {
+                context.getContentResolver().delete(WeatherEntry.CONTENT_URI,
+                        null,
+                        null);
+                context.getContentResolver().bulkInsert(WeatherEntry.CONTENT_URI,
+                        simpleJsonWeatherData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
